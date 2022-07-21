13e179e95aeaf8c494820a81b87205593301b605
Mateus Leite <mateus.lcsilva@outlook.com>
2022-07-20 23:10:43 -0300

    configuradas as rotas, melhorada a componentização

--- pdv/src/Routes/NavBarButtons/NavBarButtons.tsx
+++ pdv/src/Routes/NavBarButtons/NavBarButtons.tsx
@@ -13,7 +13,7 @@ const NavBarButtons = () => {
             <Link to="/newsalescreen"><Button text='NOVA MESA' className={(isOutlined == 1? '' : 'is-outlined') + ' btnselect is-info'} onClick={() => setIsOutlined(1)}/></Link>
             <Link to="/addsalescreen"><Button text='ACRESCENTAR PEDIDO' className={(isOutlined == 2? '' : 'is-outlined') + ' btnselect is-info'} onClick={() => setIsOutlined(2)}/></Link>
             <Link to="/updatesalescreen"><Button text='ALTERAR PEDIDO' className={(isOutlined == 3? '' : 'is-outlined') + ' btnselect is-info'} onClick={() => setIsOutlined(3)}/></Link>
-            <Link to="/checkoutscreen"><Button text='FECHAR COMANDA<' className={(isOutlined == 4? '' : 'is-outlined') + ' btnselect is-info'} onClick={() => setIsOutlined(4)}/></Link>
+            <Link to="/checkoutscreen"><Button text='FECHAR COMANDA' className={(isOutlined == 4? '' : 'is-outlined') + ' btnselect is-info'} onClick={() => setIsOutlined(4)}/></Link>
         </div>
     </>
     )
