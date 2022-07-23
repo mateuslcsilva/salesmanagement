8bf375f42aa1cf04e96f1cb18a96172266fe5652
Mateus Leite <mateus.lcsilva@outlook.com>
2022-07-22 21:43:38 -0300

    correções na ux, feitas também todas as correções nas funções e controles de estado afetados

--- pdv/src/Routes/NewSaleScreen/NewSaleScreen.tsx
+++ pdv/src/Routes/NewSaleScreen/NewSaleScreen.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './styles.css'
+import { TextField } from '@mui/material'
 import Button from '../../components/Button/Button'
-import InputNumTable from '../../components/InputNumTable/InputNumTable'
 import ItemsListInput from '../../components/ItemsListInput/ItemsListInput'
 
 
@@ -10,105 +10,147 @@ const NewSaleScreen = () => {
     interface sale {
         numTable: Number,
         numSale: Number,
+        costumerName: String,
         orders: String[],
         date: String,
         time: String
     }
 
-    const initialSale :sale = {
+    const initialSale: sale = {
         numTable: 0,
         numSale: 0,
+        costumerName: '',
         orders: [],
         date: '',
         time: ''
     }
 
     const [tableNumber, setTableNumber] = useState(0)
+    const [saleNumber, setSaleNumber] = useState(0)
+    const [costumerName, setCostumerName] = useState('')
     const [currentOrder, setCurrentOrder] = useState('')
-    const [sale, setSale] = useState({...initialSale})
+    const [sale, setSale] = useState({ ...initialSale })
 
     const setTable = () => {
         let current = new Date
-        let currentDay = current.getDate() .toString().length < 2 ? '0' + current.getDate()  : current.getDate()
-        let currentMonth = current.getMonth().toString().length < 2 ? '0' + (current.getMonth() + 1)   : (current.getMonth() + 1)
+        let currentDay = current.getDate().toString().length < 2 ? '0' + current.getDate() : current.getDate()
+        let currentMonth = current.getMonth().toString().length < 2 ? '0' + (current.getMonth() + 1) : (current.getMonth() + 1)
         let currentDate = currentDay + '/' + currentMonth + '/' + current.getFullYear()
         let currentTime = current.getHours() + ':' + current.getMinutes()
         let updatedSale = {
             numTable: tableNumber,
-            numSale: 0,
+            numSale: saleNumber,
+            costumerName: costumerName,
             orders: [],
             date: currentDate,
             time: currentTime
         }
-        setSale(sale => ({...sale, ...updatedSale}))
+        setSale(sale => ({ ...sale, ...updatedSale }))
     }
-
 
     const setOrder = () => {
         let updatedSale = {
             numTable: sale.numTable,
             numSale: sale.numSale,
+            costumerName: sale.costumerName,
             orders: [...sale.orders, currentOrder],
             date: sale.date,
             time: sale.time
         }
 
-        setSale(sale => ({...sale, ...updatedSale}))
+        setSale(sale => ({ ...sale, ...updatedSale }))
         setCurrentOrder('')
     }
 
     const clear = () => {
-        setSale({...initialSale})
+        setSale({ ...initialSale })
         setCurrentOrder('')
+        setSaleNumber(0)
         setTableNumber(0)
+        setCostumerName('')
     }
 
     useEffect(() => {
         console.log(sale)
-    }, [sale])
+    })
+
+
 
     return (
         <>
-                <div className='div' id='div1'>
-                    <h3 className='title is-3'>ABRIR COMANDA</h3>
-                    <InputNumTable 
-                    disabled={sale.numTable != 0? true : false} 
-                    onChange = {(e : any) => setTableNumber(isNaN(e.target.value)? 0 : e.target.value)} 
-                    value={tableNumber < 1 ? '' : tableNumber} 
-                    placeholder="33"
-                    className='is-info' 
-                    />
+            <div className='div' id='div1'>
+                <h3 className='title is-3'>ABRIR COMANDA</h3>
+                <TextField
+                    id="outlined-basic"
+                    label="Mesa"
+                    variant="outlined"
+                    size="small"
+                    onChange={(e: any) => setTableNumber(isNaN(e.target.value) ? 0 : e.target.value)}
+                    value={tableNumber < 1 ? '' : tableNumber}
+                    style={{ 'width': '105px' }}
+                    className='mr-2 align-right-sla'
+                />
 
-                    <Button 
-                    className='is-info ml-2' 
-                    disabled={sale.numTable != 0? true : false} 
-                    onClick={setTable} 
-                    text='Selecionar Mesa'
-                    /><br /><br/>
+                <TextField
+                    id="outlined-basic-string"
+                    label="Nome"
+                    variant="outlined"
+                    size="small"
+                    onChange={(e: any) => setCostumerName(e.target.value)}
+                    value={costumerName}
+                    style={{ 'width': '105px' }}
+                    className='mr-2'
+                />
 
-                    <ItemsListInput 
-                    className='is-info' 
+                <TextField
+                    id="outlined-basic"
+                    label="Comanda*"
+                    variant="outlined"
+                    size="small"
+                    onChange={(e: any) => setSaleNumber(isNaN(e.target.value) ? 0 : e.target.value)}
+                    value={saleNumber < 1 ? '' : saleNumber}
+                    style={{ 'width': '105px' }}
+                    className='mr-2'
+                    autoFocus 
+                />
+
+                <Button
+                    className='is-info ml-2'
+                    disabled={sale.numSale != 0 ? true : false}
+                    onClick={setTable}
+                    text='Iniciar comanda'
+                /><br /><br />
+
+                <ItemsListInput
+
+                    className='is-info'
                     placeholder="00 - Nome do Pedido"
-                    onChange = {(e : any) => setCurrentOrder(e.target.value)}
+                    onChange={(e: any) => setCurrentOrder(e.target.value)}
                     value={currentOrder}
-                    disabled = {sale.numTable == 0? true : false} 
-                    /><br /><br />
+                    disabled={sale.numSale == 0 ? true : false}
+                /><br /><br />
 
-                    <Button 
-                    onClick={setOrder} 
-                    className='is-info' 
-                    text='Acrescentar item' 
-                    disabled = {sale.numTable == 0? true : false}
-                    /><br /><br />
+                <Button
+                    onClick={setOrder}
+                    className='is-info'
+                    text='Acrescentar item'
+                    disabled={sale.numSale == 0 ? true : false}
+                /><br /><br />
 
-                    <p id='visualizacao'>{sale.numTable ? 'Mesa ' + sale.numTable + ':\n' : ''}{ sale.orders.map(item => item + '\n')}</p>
+                    {saleNumber > 0 && <p id='visualizacao'>
+                        {sale.numTable ? 'Mesa: ' + sale.numTable + '  |  ' : ''}
+                        {sale.costumerName ? 'Cliente: ' + sale.costumerName + '  |  ' : ''}
+                        {sale.numSale ? 'Comanda ' + sale.numSale + ':\n' : ''}
+                        {sale.orders.map(item => item + '\n')}
+                    </p>
+                    }
 
-                    <p className={'confirmation-text ' + (sale.orders.length > 0?'visible' : 'hidden')} >Pedido registrado!!!</p>
+                <p className={'confirmation-text ' + (sale.orders.length > 0 ? 'visible' : 'hidden')} >Pedido registrado!!!</p>
 
-                        <div className='btn-limpar-centered'>
-                            <Button onClick={clear} disabled = {sale.numTable == 0? true : false} text='Limpar'/>
-                            </div>
+                <div className='btn-limpar-centered'>
+                    <Button onClick={clear} disabled={sale.numSale == 0 ? true : false} text='Limpar' />
                 </div>
+            </div>
         </>
     )
 }
