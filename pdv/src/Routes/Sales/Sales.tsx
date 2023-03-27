49402b056b0038d41197feab163539b85496ce89
mateuslcsilva <leitemcs@hotmail.com>
2023-03-27 18:52:14 -0300

    more dashboard import and requests setted

--- pdv/src/Routes/Sales/Sales.tsx
+++ pdv/src/Routes/Sales/Sales.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { doc, getDoc } from 'firebase/firestore'
+import React, { useState } from 'react'
+import { itemType } from '../../types/itemType/itemType'
+import { sale } from '../../types/sale/sale'
+import { useAuthContext } from '../../utils/contexts/AuthProvider'
+import { useOrderContext } from '../../utils/contexts/OrderContext'
+import { db } from '../../utils/firebase/firebase'
 import './styles.css'
 
 
 export const Sales = () => {
+    const AuthContext = useAuthContext()
+    const orderContext = useOrderContext()
+    const [itemList, setItemList] = useState<Array<itemType>>([])
+    const [sales, setSales] = useState<Array<sale>>([] as Array<sale>)
+    const [salesHistory, setSalesHistory] = useState<Array<sale>>()
+
+    
+
+    const getItems = async () => {
+        if (AuthContext.currentUser.id == '') return false
+        let docRef = doc(db, "empresas", `${AuthContext.currentUser.id}`)
+        let data = await getDoc(docRef)
+            .then(res => {
+                setItemList(res.data()?.items)
+                setSales(res.data()?.sales)
+                setSalesHistory(res.data()?.salesHistory)
+            })
+    }
 
     return (
         <>
